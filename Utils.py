4885855e55f3cc66e094b6c1f3bc61bf8e4f6a87
Ghaith-Bassam-Zaza <ghaith.zaza@outlook.com>
2024-09-23 21:25:27 +0300

    created utils for NLP using NLTK3

---
+++ Utils.py
@@ -0,0 +1,18 @@
+from nltk import word_tokenize,PorterStemmer
+import numpy as np
+def tokenize(sentense):
+    return word_tokenize(sentense)
+
+stemmer = PorterStemmer()
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_word(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx,w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx]=1.0
+
+            
+    return bag

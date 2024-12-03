30ef07e31ba78e0ce4152f14d628eed8fac4e206
Ghaith-Bassam-Zaza <ghaith.zaza@outlook.com>
2024-12-03 18:12:35 +0400

    created API for bot that gets the predicted tag and gives a valid answer if the probability is high.

--- chatbot.py
+++ chatbot.py
@@ -4,15 +4,15 @@ import torch
 from json import load
 from Utils import bag_of_word, tokenize
 
-
+# get device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-
+# load dataset
 with open("./intents.json") as f:
     intents = load(f)
 
-
+# load model
 FILE = "data.pth"
 data = torch.load(FILE)
 input_size = data["input_size"]
@@ -26,24 +26,30 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
+# create bot
 bot_name = "Foulan"
 print("Let's chat! type 'quit' to exit.")
+
+# bot loop
 while True:
     sentence = input("You: ")
+    # end loop
     if sentence =="quit":
         break
-    
+    # pre-process input then push it into the model
     sentence = tokenize(sentence)
     x = bag_of_word(sentence,all_words)
     x = x.reshape(1,x.shape[0])
     x = torch.from_numpy(x).to(device)
     output = model(x)
+
+    # get output tag with probability
     _ , pridict = torch.max(output,dim = 1)
     tag = tags[pridict.item()]
-
     probs = torch.softmax(output,dim = 1)
     prob = probs[0][pridict.item()]
 
+    # if probability is high for a tag give a random answer from the tag answers, else ask user to clearify
     if prob.item() > .75:
         for intent in intents["intents"]:
             if tag == intent['tag']:
